16a1cb71f3d043ca697a4be55a5a56ae2f9f702a
AlexKing1028 <shiwenlei2012@gmail.com>
2017-03-06 20:56:41 +0800

    finish

--- sequence_alignment/src/Main.java
+++ sequence_alignment/src/Main.java
@@ -8,18 +8,23 @@ public class Main {
         List<String> ls = Reader.read("HW1FILE1.txt");
 
         int size = ls.size();
+        long time1 = 0, time2 = 0;
         for (int i=0;i<size;i++) {
             for (int j = i + 1; j < size; j++) {
                 System.out.println(i + " compared with " + j);
                 // ------  global -------
+                time1 = System.currentTimeMillis();
                 Alignment alignment = GlobalAlign.search(ls.get(i), ls.get(j));
-                System.out.println("global");
+                time1 -= System.currentTimeMillis();
+                System.out.println("global -- " + -time1);
                 System.out.println(alignment);
 //                System.out.println(alignment.a.length());
 
                 // ------ local ------
-                System.out.println("local");
+                time2 = System.currentTimeMillis();
                 alignment = LocalAlign.search(ls.get(i), ls.get(j));
+                time2 -= System.currentTimeMillis();
+                System.out.println("local -- " + -time2);
                 System.out.println(alignment);
 //                System.out.println(alignment.a.length());
             }
